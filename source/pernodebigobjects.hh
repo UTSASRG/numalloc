dc6682c1a02a19220935b935a789da89fa86f0b6
tongping <tongpingliu@gmail.com>
2019-12-21 17:55:43 -0500

    Removing linklist from pernodebigobjects.hh

--- source/pernodebigobjects.hh
+++ source/pernodebigobjects.hh
@@ -2,7 +2,6 @@
 #define __PER_NODE_BIG_OBJECTS_HH__
 
 #include <pthread.h>
-#include "dlist.hh"
 #include "xdefines.hh"
 #include "perthread.hh"
 
@@ -14,7 +13,6 @@ class PerNodeBigObjects {
   // the freelist that is ordered by the deallocation order.
   class PerBigObject {
   public:
-    list_t list;
     void *  address;
     size_t  size;
   };
@@ -29,15 +27,13 @@ class PerNodeBigObjects {
   };
 
 private:
-  // Pointing to freed big objects
-  list_t _list;
-
   // A circular array with the maximum of 1024 objects;
   // When an object is used, it will be removed from the list. 
   // Also, the corresponding BigObjectInfo will be erased. 
   PerBigObject * _objects;
   void * _heapBegin;
 
+  // TODO: put this in the upper level
   //[PER_NODE_MAX_BIG_OBJECTS];  
   pthread_spinlock_t _lock; 
   
@@ -46,7 +42,6 @@ private:
 
   // The size of total freed objects of this node
   unsigned long _totalSize; 
- // unsigned long _nindex; 
 
   // Place the PerMBInfo in this object, since everytime when the information will be changed
   // by each allocation and deallocation. If the information is placed in pernodeheap, although
@@ -62,11 +57,7 @@ public:
     _max = PER_NODE_MAX_BIG_OBJECTS - 1;
     
     _heapBegin = heapBegin;
-    //fprintf(stderr, "_heapBegin %p is at %p size is %ld. _max is at %p\n", _heapBegin, &_heapBegin, sizeof(_heapBegin), &_max); 
-    //_nindex = ??;
 
-    // Initialize the _list and _lock
-    listInit(&_list); 
     pthread_spin_init(&_lock, 0);
 
     // Initialize the _objects array
@@ -98,14 +89,10 @@ public:
 
     lock();
 
-    
-    // Now search the freelist to find an entry that satisfies the request
-    list_t * entry = _list.next;
+    for(int i = _next - 1; i>= 0; i--) {
+      PerBigObject * object = &_objects[i];   
+    // Now search the objects to find an entry that satisfies the request
   //  fprintf(stderr, "AllocateBigObject totalSize %lx request size %lx\n",  _totalSize, size);
-
-    do {
-      PerBigObject * object = (PerBigObject *)entry; 
-
       // Check whether the current entry can be merged with its neibour to satisfy this request
       if(object->size < size) {
 #if 0
@@ -137,25 +124,12 @@ public:
         object->size -= size; 
         _totalSize -= size;
 
-        //fprintf(stderr, "line %d: get size. Object->size %ld totalsize %ld\n", __LINE__, object->size, _totalSize);
         // Unusual case: the object size is the requested size, then we should change the freelist
         if (object->size == 0) {
-          //fprintf(stderr, "_next %ld: the object is freed right now. entry %p, lastentry %p\n", _next, entry, &_objects[_next - 1]);
-          // When the current entry is not the last entry,
-          // then we try to copy the last entry to the current entry, in order to free one entry
-          if(entry != (list_t *)&_objects[_next - 1]) {
-            // Copy the last entry here
-            memcpy(entry, (void *)&_objects[_next - 1], sizeof(PerBigObject));
+            
+          object->size = _objects[_next - 1].size;
+          object->address = _objects[_next -1].address;
 
-            // Update the last entry's prev and next node, so that they can be pointed to the current entry
-            // Note that there is no need to change the list pointers for the current entry
-            listUpdateEntry(entry);
-          }
-          else { 
-            // Remove this entry from the list.
-            listRemoveNode(entry); 
-          }
-          
           // Freed an entry
           _next--;
         }
@@ -176,23 +150,14 @@ public:
           // Change the remainning one
           changePerMBInfoSize(object->address, newSize, newSize);
         }
-
         break;
       }
-
-      // If the current entry cannot be allocated (due to a smaller size), try next entry 
-//     fprintf(stderr, "entry %p entry->next %p head %p\n", entry, entry->next, &_list);
-      entry = entry->next; 
-      if(isListTail(entry)) {
-        break;
-      }
-      // Now check whether entry is the last entry
-    } while (true);
+    } 
 
     unlock();
 
-  //  if(ptr)
-  //  fprintf(stderr, "GET object ptr %p size %lx entry %p _next %ld\n", ptr, size, entry, _next);
+  if(ptr)
+    fprintf(stderr, "GET object ptr %p size %lx _next %ld\n", ptr, size, _next);
 
     return ptr; 
   }
@@ -275,7 +240,6 @@ public:
 
     PerBigObject * object = &_objects[_next];
     
-    nodeInit(&object->list);
     object->size = size; 
     object->address = ptr;
 
@@ -285,27 +249,20 @@ public:
   
     assert(_next != _max); 
   
-    // Insert this entry to the header of freelist. 
-    listInsertHead(&object->list, &_list);
-    //insertDLBegin(&_list, &object->list);
-
     // Change PerMBInfo in order to encourage the coalesce TODO:  
     clearPerMBInfo(ptr, size);
 
-      unlock();
+    unlock();
   
-   //   fprintf(stderr, "deallocate %p with size %lx: object %p prev: %p,next:%p.  _next:%ld\n", ptr, size, object, object->list.prev, object->list.next, _next);
   }
 
 private:
   void lock() {
       pthread_spin_lock(&_lock);
-//      fprintf(stderr, "lock.  thread-id:%d\n", current->index);
   }
   
   void unlock() {
       pthread_spin_unlock(&_lock);
-//      fprintf(stderr, "unlock.  thread-id:%d\n", current->index);
   }
 
 };
