17a649166ac38ac877461a08a9794a57c9a23f25
tongping <tongpingliu@gmail.com>
2020-01-06 11:17:31 -0500

    Fastest version with facesim around 49seconds.

---
+++ source-fastest/mainheap.hh
@@ -0,0 +1,304 @@
+#ifndef __MAIN_HEAP_HH__
+#define __MAIN_HEAP_HH__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "xdefines.hh"
+#include "mm.hh"
+#include "pernodebigobjects.hh"
+#include "real.hh"
+#include "perthread.hh"
+
+/* Basic memory mechanism for main thread.
+ 
+   The memory management of the main thread is different from other threads,
+   mainly because the main thread typically prepare the data for its children 
+   threads. Therefore, the memory allocation of the main thread will come from two 
+   separate spans that are not always allocated locally. 
+   In fact, we are more careful about the load balance issue, due to the fact that all children threads
+   are sharing with it.
+
+   However, if an object is larger than PAGE*NUMA_NODES, then we will utilize block-wise 
+   allocations, which has been observed by many existing work (such as Xu's PPoPP'14).
+  
+   Similar to other nodes' allocation, the allocation of main thread will be from two spans. 
+   The first half of the perfthreadheap will be completely interleaved, while 
+   the second half will be block-wise interleaved method.
+
+   For simplicity, all these objects will be returned to this heap, but will be returned to 
+   the os if it is larger than the bag size after the creation of threads.
+   We don't want to create unnecessary remote accesses. 
+
+   The allocation of this heap can be only used if there is one thread. In fact, it is important to 
+   support this for some openmp program
+*/
+
+class MainHeap {
+  
+  class MainThreadSizeClass {
+  private:
+    unsigned int _csize;
+    unsigned int _sc;
+    unsigned int _pages;
+    unsigned int _nodeindex;
+    char        * _bumpPointer;
+    char        * _bumpPointerEnd;
+    FreeList     _flist;
+
+  public:
+    
+    void initialize(unsigned int sc, unsigned int classsize, unsigned int nodeindex) {
+      _sc = sc;
+      _csize = classsize;
+      _nodeindex = nodeindex;
+      _pages = classsize >> PAGE_SIZE_SHIFT;
+      _bumpPointer = NULL;
+      _bumpPointerEnd = NULL;
+      _flist.Init();
+    }
+
+    bool hasObject() {
+      return ((_flist.length() > 0) || (_bumpPointer < _bumpPointerEnd));
+    }
+
+    // The current object will be pushed with a new block of memory
+    void updateBumpPointer(char * start, size_t size) {
+      _bumpPointer = start;
+      _bumpPointerEnd = start + size;
+    }
+
+    unsigned int getClassSize() {
+      return _csize;
+    }
+
+    void * allocate(void) {
+      void * ptr = NULL; 
+
+      // Allocate from the freelist first.
+      if(_flist.hasItems()) { 
+        ptr = _flist.Pop(); 
+      }
+      else { 
+        // Allocate from the bump pointer right now. 
+        ptr = _bumpPointer;
+        _bumpPointer += _csize;
+
+        if(_csize > (PAGE_SIZE * NUMA_NODES/2)) {
+          // Notify the OS to allocate physical memory in the block-wise way
+          MM::bindMemoryBlockwise((char *)ptr, _pages, _nodeindex);
+        }
+      }
+
+      return ptr;
+    }
+
+    void deallocate(void * ptr) {
+      _flist.Push(ptr);
+    }
+  }; 
+    
+
+  private:
+    char * _begin;
+    char * _end;
+   
+    unsigned int  _nodeindex;
+    bool    _mainHeapPhase; 
+    size_t  _scMagicValue; 
+
+    char * _bpSmall;
+    char * _bpSmallEnd;
+    char * _bpBig;
+    char * _bpBigEnd;
+    pthread_spinlock_t _lock;
+
+    // Currently, size class is from 2^4 to 2^19 (512 KB), with 16 sizes in total.
+    MainThreadSizeClass _sizes[SMALL_SIZE_CLASSES];
+
+    // _bigObjects will also maintain the PerMBINfo
+    PerNodeBigObjects _bigObjects;
+
+ public:
+
+   void initialize(int nodeindex, void * begin) {
+      size_t heapsize = 2 * SIZE_PER_SPAN;
+
+     // Save the heap related information
+      _begin = (char *)begin;
+      _end = _begin + heapsize;
+
+      //_bpSmall = (char *)MM::mmapFromNode(SIZE_PER_SPAN, nodeindex, (void *)begin, false);
+    //  _bpSmall = (char *)MM::mmapAllocatePrivate(SIZE_PER_SPAN, (void *)begin);
+      _bpSmall = (char *)MM::mmapPageInterleaved(SIZE_PER_SPAN, (void *)begin);
+      _bpSmallEnd = _bpSmall + SIZE_PER_SPAN;
+
+      // Initialize the big heap. Differently, we don't do mmap at first, since 
+      // the only way to use huge page is through mmap. Then we will do the allocation 
+      // on demand, based on the requirement.
+      _bpBig = _bpSmallEnd;
+      _bpBigEnd = _bpBig + SIZE_PER_SPAN;  
+
+      _nodeindex = nodeindex; 
+      _scMagicValue = 32 - LOG2(SIZE_CLASS_START_SIZE);
+     
+      // Compute the metadata size for PerNodeHeap
+      size_t metasize = _bigObjects.computeImplicitSize(heapsize);
+
+      // Binding the memory to the specified node.
+      char * ptr = (char *)MM::mmapFromNode(alignup(metasize, PAGE_SIZE), nodeindex);
+      _bigObjects.initialize(ptr, _bpSmall, SIZE_PER_SPAN*2);
+
+      // Initialize all size classes. 
+      unsigned long classSize = 16;
+      for(int i = 0; i < SMALL_SIZE_CLASSES; i++) {
+        // Each element of
+        _sizes[i].initialize(i, classSize, nodeindex);
+
+        classSize *= 2;
+      }
+
+      // Initialize the lock
+      pthread_spin_init(&_lock, PTHREAD_PROCESS_PRIVATE);
+   } 
+
+   inline size_t getSize(void * ptr) {
+      return _bigObjects.getSize(ptr);
+   } 
+
+  inline int getSizeClass(void * ptr) {
+    size_t size = getSize(ptr); 
+   // fprintf(stderr, "getSizeClass ptr %p size %lx\n", ptr, size); 
+    return getSizeClass(size);
+  }
+
+
+  inline int getSizeClass(size_t size) {
+    if(size <= 16) {
+      return 0;
+    }
+    else {
+      return _scMagicValue - __builtin_clz(size - 1);
+    }
+  }
+
+  // allocate a big object with the specified size
+  void * allocateBigObject(size_t size) {
+    void * ptr = NULL;
+    size = alignup(size, SIZE_ONE_MB);
+
+    // Allocate from the free list at first
+    ptr = _bigObjects.allocate(size);
+    if(ptr == NULL) {
+      // Now allocate from the bump pointer
+      allocateFromBigBumppointer(size);
+    }
+    return ptr;
+  }
+
+  void * allocateFromBigBumppointer(size_t sz) {
+      void * ptr = NULL;
+      bool isHugePage = false;
+
+      size_t size = sz;
+      ptr = _bpBig;
+      size_t pages = size/PAGE_SIZE;
+
+      // If it is possible to use huge pages
+      if(size > SIZE_HUGE_PAGE * (NUMA_NODES-1)) {
+        // Get the number of huge pages at first
+        pages = size>>SIZE_HUGE_PAGE_SHIFT;
+        if((size & SIZE_HUGE_PAGE_MASK) != 0) {
+          pages += 1;
+        }
+
+        // Compute the blockSize. We will try to be balanced.
+        // Overall, we don't want one node that has 1 more blocks than the others.
+        size = pages * SIZE_HUGE_PAGE;
+        isHugePage = true;
+
+       // fprintf(stderr, "Size is %lx _bpBig %p ptr %p\n", size, _bpBig, ptr);
+        ptr = (void *)alignup((uintptr_t)ptr, SIZE_HUGE_PAGE);
+        //fprintf(stderr, "BigObject allocate: size %lx, pages %lx, ptr %p\n", sz, pages, ptr);
+        _bpBig = (char *)ptr;
+     }
+    
+     //fprintf(stderr, "allocate size %lx _bpBig %p ptr %p\n", size, _bpBig, ptr); 
+     _bpBig += size;
+
+     // Allocate the memory from the OS
+     ptr = MM::mmapAllocatePrivate(size, ptr, isHugePage);
+
+     // Now binding the memory to different nodes.
+     MM::bindMemoryBlockwise((char *)ptr, pages, _nodeindex, isHugePage);
+
+     // Mark the object size
+     markPerMBInfo(ptr, size, size);
+ 
+     return ptr;
+   }
+
+   void markPerMBInfo(void * blockStart, size_t blockSize, size_t size) {
+     _bigObjects.markPerMBInfo(blockStart, blockSize, size);
+   }
+
+   void * allocate(size_t size) {
+     if(isBigObject(size)) {
+       //fprintf(stderr, "Allocate big object with size %lx\n", size);
+       return allocateBigObject(size);
+     }
+     else {
+       return allocateSmallObject(size);
+     }
+   }
+
+   inline bool isBigObject(size_t size) {
+      return (size >= BIG_OBJECT_SIZE_THRESHOLD) ? true : false;
+   }
+
+   void * allocateSmallObject(size_t size) {
+    int sc; 
+
+    // If it is small object, allocate from the freelist at first.
+    sc = getSizeClass(size);
+    if(!_sizes[sc].hasObject()) {
+      void * ptr = _bpSmall;
+      _bpSmall += SIZE_ONE_MB;
+      _sizes[sc].updateBumpPointer((char *)ptr, SIZE_ONE_MB);
+
+      //fprintf(stderr, "allocate small object\n");
+      // Update the PerMBInfo as well
+      markPerMBInfo(ptr, SIZE_ONE_MB, _sizes[sc].getClassSize()); 
+    }
+
+    return _sizes[sc].allocate();
+  }
+
+   void deallocate(void * ptr) {
+     size_t size = getSize(ptr);
+     if(!isBigObject(size)) {
+       int sc = getSizeClass(size); 
+      
+      if(!_mainHeapPhase) { 
+        pthread_spin_lock(&_lock);
+        // Return to the size class
+        _sizes[sc].deallocate(ptr);
+        pthread_spin_unlock(&_lock);
+      }
+      else {
+        _sizes[sc].deallocate(ptr);
+      }
+     }
+     else {
+      if(!_mainHeapPhase) { 
+        pthread_spin_lock(&_lock);
+        _bigObjects.deallocate(ptr, size);
+        pthread_spin_unlock(&_lock);
+      }
+      else {
+        _bigObjects.deallocate(ptr, size);
+      }
+     }
+   }
+ 
+};
+#endif
---
+++ source-fastest/xthread.hh
@@ -0,0 +1,284 @@
+/*
+ * FreeGuard: A Faster Secure Heap Allocator
+ * Copyright (C) 2017 Sam Silvestro, Hongyu Liu, Corey Crosser, 
+ *                    Zhiqiang Lin, and Tongping Liu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * 
+ * @file   xthread.hh: thread-related functions implementation.
+ * @author Tongping Liu <http://www.cs.utsa.edu/~tongpingliu/>
+ * @author Sam Silvestro <sam.silvestro@utsa.edu>
+ */
+#ifndef __XTHREAD_HH__
+#define __XTHREAD_HH__
+
+#include <unistd.h>
+#include <new>
+#include <sys/syscall.h>
+#include <signal.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
+#include <errno.h>
+#include "log.hh"
+#include "perthread.hh"
+#include "real.hh"
+#include "xdefines.hh"
+
+
+class xthread {
+#define MAX_ALIVE_THREADS 256
+
+	public:
+  static inline xthread& getInstance() {
+    static char buf[sizeof(xthread)];
+    static xthread* xthreadObject = new (buf) xthread();
+    return *xthreadObject;
+  }
+
+	void initialize() {
+		  _alives = 0;
+      _next = 0;
+      _max = MAX_ALIVE_THREADS - 1;
+
+	    // Initialize the spin_lock
+	    pthread_spin_init(&_spin_lock, PTHREAD_PROCESS_PRIVATE);
+
+
+      // Shared the threads information.
+      memset(&_threads, 0, sizeof(_threads));
+
+      _nodeIndex = getRealNodeIndex();
+      _nodeMax = NUMA_NODES;
+
+
+      int num_cpus = get_nprocs();
+
+      cpu_set_t *cpusetp;
+      size_t size;
+
+      cpusetp = CPU_ALLOC(num_cpus);
+      if (cpusetp == NULL) {
+        perror("CPU_ALLOC");
+        exit(EXIT_FAILURE);
+      }
+
+      size = CPU_ALLOC_SIZE(num_cpus);
+
+      int num_cpus_per_node = num_cpus/NUMA_NODES;
+      int cpu_now = 0; 
+      // Making sure that the configuration is the same as the results of lscpu
+      // Initialize the CPU set!! 
+      for(int i = 0; i < NUMA_NODES; i++) {
+//        fprintf(stderr, "i%d cpu_now %d num_cpus_pernode %d\n", i, cpu_now, num_cpus_per_node);
+        CPU_ZERO_S(size, cpusetp);
+        for (int cpu = cpu_now; cpu < cpu_now + num_cpus_per_node; cpu++) {
+//          fprintf(stderr, "Node %d: setcpu %d\n", i, cpu);
+          CPU_SET_S(cpu, size, cpusetp);
+        }
+        pthread_attr_setaffinity_np(&_tattrs[i], size, cpusetp);
+        cpu_now += num_cpus_per_node;
+      }
+      
+      // Initialize all threads's structure at once 
+      thread_t * thread;
+      for(int i = 0; i < MAX_ALIVE_THREADS; i++) {
+        thread = &_threads[i];
+
+			  // Those information that are only initialized once.
+     	  thread->available = true;
+        thread->hasChildrenthreads = false;
+        thread->index = i;
+
+        // We will assign the thread to each node during the initialization.
+        // There is no need to re-assign later 
+        thread->nindex = _nodeIndex;
+        thread->inited = false;
+
+        // Always makes the first thread to be the same node as the main thread
+        if(i != 0) {
+          _nodeIndex++;
+          if(_nodeIndex == _nodeMax) {
+            _nodeIndex = 0;
+          }
+        }
+      //  fprintf(stderr, "initialize i %d _nodeIndex %d\n", i, _nodeIndex);
+      }
+
+		  // Now we will intialize the initial thread
+		  initializeInitialThread();
+  }
+
+
+  void finalize() {}
+
+  
+	void initializeInitialThread(void) {
+		int tindex = allocThreadIndex();
+   	assert(tindex == 0);
+
+    //getThread(tindex);
+    initializeCurrentThread(getThread(tindex));
+    
+		// Adding the thread's pthreadt.
+		current->thread = pthread_self();
+
+    // Binding the current thread to the current node.    
+    if(numa_run_on_node(current->nindex) != 0) {
+      fprintf(stderr, "Binding initial task to node %d failed with error %s\n", current->nindex, strerror(errno));
+      exit(-1);
+    }
+  }
+
+	// This function is only called in the current thread before the real thread function 
+	void initializeCurrentThread(thread_t * thread) {
+    current = thread;
+		current->tid = syscall(__NR_gettid);
+    int realNodeIndex = getRealNodeIndex();
+  // fprintf(stderr, "Thread index %d nodeindex %d actual node %d\n", current->index, current->nindex, getRealNodeIndex());
+    if(current->nindex != realNodeIndex) {
+      fprintf(stderr, "Thread index %d nodeindex %d actual node %d\n", current->index, current->nindex, getRealNodeIndex());
+      current->nindex = realNodeIndex;
+    }
+
+    // Initialize the current heap
+    if(current->inited == false) 
+      initCurrentHeap(current->index, current->nindex); 
+
+    //current->startFrame = (char *)__builtin_frame_address(0);
+  }
+
+	/// @ internal function: allocation a thread index when spawning.
+  int allocThreadIndex() {
+    int index = -1;
+    thread_t* thread;
+
+    lock();
+
+    if(_alives == _next) {
+      // All entries before _next has been utilized -- common case
+			index = _next;
+      _next++;
+      thread = getThread(index);
+			thread->available = false;
+		} 
+    else {
+      // Some threads have exited before. 
+			for(int i = 0; i <= _next; i++) {
+      	thread = getThread(i);
+      	if(thread->available) {
+        	thread->available = false;
+					index = i;
+					break;
+				}
+			}
+		}
+    // Now we will create one more thread
+    _alives++;
+
+   if(_alives >= _max) {
+    fprintf(stderr, "alives is %d _max is %d\n", _alives, _max); 
+    assert(_alives < _max);
+   }
+
+		unlock();
+    return index;
+  }
+
+  inline thread_t* getThread(int index) { return &_threads[index]; }
+
+
+  static void * startThread(void * arg) {
+    thread_t * current = (thread_t *) arg;
+
+    xthread::getInstance().initializeCurrentThread(current);
+
+    return current->startRoutine(current->startArg);
+  }
+
+  int findJoineeIndex(pthread_t thread) {
+    int index = -1; 
+    for(int i = 0; i < _max; i++) {
+      // Exit if we find the entry in _threads array
+      if(_threads[i].thread == thread) {
+        index = i; 
+        break;
+      }
+    }
+
+    if(index == -1) {
+      fprintf(stderr, "Can't find the thread?? \n");
+      abort();      
+    }
+    return index; 
+  }
+
+	int thread_create(pthread_t * thread, const pthread_attr_t * attr, threadFunction * fn, void * arg);
+	int thread_join(pthread_t thread, void ** retval);
+
+	void lock() {
+		spin_lock();
+	}
+	
+	void unlock() {
+		spin_unlock();
+	}
+	
+private:
+
+  void initCurrentHeap(int tindex, int nindex) {
+    // Allocate a big chunk of memory from the local node
+    size_t size = LOG_SIZE + sizeof(PerThreadHeap); 
+    size = alignup(size, PAGE_SIZE);
+
+    //fprintf(stderr, "InitCurrentHeap with Size %lx", size);
+    char * ptr = (char *)MM::mmapFromNode(size, current->nindex);
+    if(ptr == 0) { 
+      fprintf(stderr, "InitCurrentHeap thread %d: with ptr %p\n", tindex, ptr);
+      assert(ptr != 0);
+    }
+
+    current->outputBuf = (char *)ptr;
+    ptr += LOG_SIZE;
+
+    current->ptheap = (PerThreadHeap *)ptr;
+
+    // Initialize the current thread's heap
+    current->ptheap->initialize(tindex, nindex);
+    current->inited = true;
+  }
+
+	inline void spin_lock() {
+		pthread_spin_lock(&_spin_lock);
+	} 
+
+	inline void spin_unlock() { 
+		pthread_spin_unlock(&_spin_lock); 
+	}
+
+	pthread_spinlock_t _spin_lock;
+
+  // The next available thread index 
+  int _next;
+  int _max;
+	int _alives;
+
+  // Bind different threads to different nodes in a circular way
+  int  _nodeMax; 
+  int  _nodeIndex;
+  pthread_attr_t _tattrs[NUMA_NODES];
+  thread_t _threads[MAX_ALIVE_THREADS];
+};
+#endif
