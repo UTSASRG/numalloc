2d4a9840ef1959ea7c19c21662c64211c7a11e01
XIN ZHAO <xinzhao@ssid-umass-vl922-172-30-22-10.wireless.umass.edu>
2020-01-30 15:35:29 -0500

    threads assignment

--- source/xthread.hh
+++ source/xthread.hh
@@ -36,6 +36,7 @@
 #include "perthread.hh"
 #include "real.hh"
 #include "xdefines.hh"
+#include "numas.hh"
 
 
 class xthread {
@@ -78,9 +79,19 @@ class xthread {
       size = CPU_ALLOC_SIZE(num_cpus);
 
       int num_cpus_per_node = num_cpus/NUMA_NODES;
-      int cpu_now = 0; 
       // Making sure that the configuration is the same as the results of lscpu
-      // Initialize the CPU set!! 
+      // Initialize the CPU set!!
+        for (int i = 0; i < NUMA_NODES; i++) {
+            struct logic_cpu cpus = Numa::get_logic_cpu(i);
+            CPU_ZERO_S(size, cpusetp);
+            for (int j = 0; j < cpus.size; j++) {
+//                fprintf(stderr, "Node %d: setcpu %d\n", i, cpus.cpus[j]);
+                CPU_SET_S(cpus.cpus[j], size, cpusetp);
+            }
+            pthread_attr_setaffinity_np(&_tattrs[i], size, cpusetp);
+        }
+#ifdef false
+        int cpu_now = 0;
       for(int i = 0; i < NUMA_NODES; i++) {
 //        fprintf(stderr, "i%d cpu_now %d num_cpus_pernode %d\n", i, cpu_now, num_cpus_per_node);
         CPU_ZERO_S(size, cpusetp);
@@ -91,6 +102,7 @@ class xthread {
         pthread_attr_setaffinity_np(&_tattrs[i], size, cpusetp);
         cpu_now += num_cpus_per_node;
       }
+#endif
       
       // Initialize all threads's structure at once 
       thread_t * thread;
