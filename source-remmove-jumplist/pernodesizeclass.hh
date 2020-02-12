468b35b32e10b78c963c175affff62803a246ce1
XIN ZHAO <xinzhao@1x-nat-vl932-172-30-228-190.wireless.umass.edu>
2020-02-11 19:54:33 -0500

    old version

---
+++ source-remmove-jumplist/pernodesizeclass.hh
@@ -0,0 +1,98 @@
+#ifndef __PER_NODE_SIZE_CLASS_HH__
+#define __PER_NODE_SIZE_CLASS_HH__
+
+#include "mm.hh"
+#include "freelist.hh"
+#include "perthread.hh"
+
+// For small objects, each size class will maintain
+// one bumppointer and one freelist
+class PerNodeSizeClass {
+  private: 
+    pthread_spinlock_t _lock; 
+    unsigned long _size; // Size of this size class
+    FreeList _flist;
+
+ public:
+    void initialize(unsigned long size) {
+      // Initialize the lock
+      pthread_spin_init(&_lock, PTHREAD_PROCESS_PRIVATE);
+
+      _size = size;
+
+      // Initialize the freelist
+      _flist.Init();
+    }  
+
+    // Allocate multiple objects in a batch from the pernode's size class 
+    int allocateBatch(unsigned long requestNumb, void ** head, void ** tail) {
+      int  numb = 0;
+
+      lock();
+      
+      numb = _flist.length(); 
+      if(numb > requestNumb) {
+        numb = requestNumb;
+      }
+      
+      //#fprintf(stderr, "size %ld: Move batch from the pernode list.\n", _size); 
+      // Pop the specified number of entries;
+      if(numb > 0)  
+      _flist.PopRange(numb, head, tail); 
+      //fprintf(stderr, "size %ld: Move batch %d from the pernode list.\n", _size, numb); 
+           
+      unlock();
+
+      return numb;
+    }
+    
+    void * allocate(void) {
+      void * ptr = NULL;
+     
+      lock();
+      if(_flist.hasItems()) 
+        ptr = _flist.Pop();
+      unlock();
+
+      return ptr;
+    }
+
+
+    void deallocate(void * ptr) {
+      lock();
+      _flist.Push(ptr);
+      unlock();
+    }
+
+
+    // Deallocate multiple objects to the pernode's size class. 
+    void deallocateBatch(unsigned long numb, void *head, void * tail) {
+      lock();
+     
+      //fprintf(stderr, "Donate batch to the pernode lsit.\n"); 
+      _flist.PushRange(numb, head, tail);
+       
+      unlock();
+
+      return;
+    }
+
+    inline bool hasItems() {
+      return _flist.hasItems();
+    }
+
+    inline size_t getSizeClass() {
+      return _size;
+    }
+
+ private:
+    void lock() {
+      pthread_spin_lock(&_lock);
+    }
+
+    void unlock() {
+      pthread_spin_unlock(&_lock);
+    }
+};
+
+#endif
---
+++ source-remmove-jumplist/perthreadsizeclass.cpp
@@ -0,0 +1,107 @@
+
+#include "mm.hh"
+#include "perthreadsizeclass.hh"
+#include "numaheap.hh"
+
+// Allocate reversely. Since _next always points to
+// the next availabe slot, we will use the object pointed by _next-1
+void * PerThreadSizeClass::allocateFromFreelist() {
+  return _flist.Pop();
+}
+
+/*
+    void *tail, *head;
+    src->PopRange(batch_size, &head, &tail);
+    central_cache[cl].InsertRange(head, tail, batch_size);
+*/
+int PerThreadSizeClass::moveObjectsFromNodeFreelist() {
+  int ret;
+  void * head, * tail;
+
+  // Get the objects from the node's freelist. 
+  ret = NumaHeap::getInstance().moveBatchFromNodeFreelist(_nodeindex, _sc, _batch, &head, &tail);
+
+  // Now place these objects to the freelist
+  if(ret > 0)
+    _flist.PushRange(ret, head, tail);
+
+  return ret;
+}
+
+void PerThreadSizeClass::donateObjectsToNodeFreelist() {
+  void * head, * tail;
+  
+  _flist.PopRange(_batch, &head, &tail);
+
+  // Donate objects to the node's freelist
+  //fprintf(stderr, "Thread %d :donate batch %d to node list, remain objects %ld\n", getNodeIndex(), _batch, _flist.length());
+  NumaHeap::getInstance().donateBatchToNodeFreelist(_nodeindex, _sc, _batch, head, tail);
+}
+
+void * PerThreadSizeClass::allocate() {
+    void * ptr = NULL; 
+    if(_flist.hasItems()) {
+      ptr = allocateFromFreelist();
+      if(ptr == NULL) {
+        fprintf(stderr, "_size is %d _sc %d, ptr %p\n", _size, _sc, ptr);
+      }
+      return ptr; 
+    }
+      
+    if(_allocs >= _allocsBeforeCheck) {
+      _allocs = 0;
+   
+      // If no available objects, allocate objects from the node's freelist 
+      int numb = moveObjectsFromNodeFreelist();
+
+      // Now let's place this list to the current list. 
+      if(numb > 0) {
+        ptr = allocateFromFreelist(); 
+        if(numb == _batch) {
+          // Since we could get the objects, let's reduce the threshold for getting from the global list
+          _allocsBeforeCheck /= 2;
+          if(_allocsBeforeCheck <= _allocsCheckMin) {
+            _allocsBeforeCheck = _allocsCheckMin;
+          }
+        }
+      }
+      else {
+        // Can't find available objects in PerNodeFreeList, then we will 
+        // check less frequently. 
+        _allocsBeforeCheck *= 2;
+        if(_allocsBeforeCheck > _allocsCheckMax) {
+          _allocsBeforeCheck = _allocsCheckMax;
+        }
+      }
+    }
+    
+    if(ptr == NULL) {
+      _allocs++;
+   
+      // Get a new block if necessary 
+      if(_bumpPointer == _bumpPointerEnd) {
+        // Now get a chunk from the current PerNodeHeap: either from big objects or never allocated ones
+        _bumpPointer = (char *)NumaHeap::getInstance().allocateOneBagFromNode(getNodeIndex(), _size, _bagSize); 
+        _bumpPointerEnd = _bumpPointer + _bagSize;
+      }
+
+      // Now allocate from the never used ones
+      ptr = _bumpPointer;
+      _bumpPointer += _size;
+    }
+    return ptr;
+  }
+ 
+  // Return an object starting with ptr to the PerThreadSizeClass
+  void PerThreadSizeClass::deallocate(void *ptr) {
+    // Put this entry to the list. 
+    _flist.Push(ptr);
+
+    // If there is no spot to hold next deallocation, put some objects to PerNodeSizeClass 
+    if(_flist.length() >= _watermark) {
+      // Donate the _batch items to the pernodefreelist 
+      donateObjectsToNodeFreelist();
+    }
+
+    return; 
+  }
