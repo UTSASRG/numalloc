3355d041afb419f2c9b934049515ab1035441f5f
XIN ZHAO <xinzhao@XINdeMacBook-Pro.local>
2020-04-21 21:44:26 -0400

    log

--- binding/xthread.hh
+++ binding/xthread.hh
@@ -79,6 +79,7 @@ class xthread {
 #ifndef MAX_PROCESSOR
         totalCpus = get_nprocs();
 #endif
+        fprintf(stderr, "max processor is  %d\n", totalCpus);
       cpu_set_t *cpusetp;
       size_t size;
 
